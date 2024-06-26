60f8745fd9c30b9e0545ac41674d46e89518dd6c
Jared <jared@Zipcoders-MacBook-Pro-3.local>
2024-06-26 14:58:18 -0400

    added new csv, getters and setters, and modified toString

--- src/main/java/zipcode/rocks/Person.java
+++ src/main/java/zipcode/rocks/Person.java
@@ -1,32 +1,69 @@
 package zipcode.rocks;
 
 public class Person {
-    private String firstname;
-    private String lastname;
+    private String name;
+    private String favoritebeverage;
+    private String favoritecolor;
+    private String birthmonth;
+    private String havepets;
 
-    public Person(String fname, String lname) {
-        this.firstname = fname;
-        this.lastname = lname;
+    public Person(String name, String favoritebeverage, String favoritecolor, String birthmonth, String havepets) {
+        this.name = name;
+        this.favoritebeverage = favoritebeverage;
+        this.favoritecolor = favoritecolor;
+        this.birthmonth = birthmonth;
+        this.havepets = havepets;
+
     }
 
-
-    public String getFirstname() {
-        return firstname;
+    public String getName() {
+        return name;
     }
 
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public String getLastname() {
-        return lastname;
+    public String getfavoritebeverage() {
+        return favoritebeverage;
     }
 
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
+    public void setfavoritebeverage(String favoritebeverage) {
+        this.favoritebeverage = favoritebeverage;
     }
 
+    public String getfavoritecolor() {
+        return favoritecolor;
+    }
+
+    public void setfavoritecolor(String favoritecolor) {
+        this.favoritecolor = favoritecolor;
+    }
+
+    public String getbirthmonth() {
+        return birthmonth;
+    }
+
+    public void setbirthmonth(String birthmonth) {
+        this.birthmonth = birthmonth;
+    }
+
+    public String gethavepets() {
+        return havepets;
+    }
+
+    public void sethavepets(String havepets) {
+        this.havepets = havepets;
+    }
+
+    @Override
     public String toString() {
-        return this.lastname + ", " + this.firstname;
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", favoritebeverage='" + favoritebeverage + '\'' +
+                ", favoritecolor='" + favoritecolor + '\'' +
+                ", birthmonth='" + birthmonth + '\'' +
+                ", havepets='" + havepets + '\'' +
+                '}';
     }
 }
